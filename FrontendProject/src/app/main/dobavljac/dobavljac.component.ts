66cc31b8ecb1d0d6026fe336834eb5313ff60769
staka99 <marko.staka@uns.ac.rs>
2026-05-12 14:35:44 +0200

    Vezbe 6 - 12. 5. 2026 - Angular aplikacija, kreiranje komponenti

---
+++ FrontendProject/src/app/main/dobavljac/dobavljac.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dobavljac',
+  imports: [],
+  templateUrl: './dobavljac.component.html',
+  styleUrl: './dobavljac.component.css'
+})
+export class DobavljacComponent {
+
+}
---
+++ FrontendProject/src/app/main/porudzbina/porudzbina.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-porudzbina',
+  imports: [],
+  templateUrl: './porudzbina.component.html',
+  styleUrl: './porudzbina.component.css'
+})
+export class PorudzbinaComponent {
+
+}
